a891bdcc2364cdff26101bed2fb4d365d367b865
Rohit <rkumar8092378845@gmail.com>
2021-06-15 08:38:50 +0530

    -> Initialized the adapter in MainActivity.kt and set up the viewpager

--- app/src/main/java/com/rohitthebest/imagesliderusingviewpager2/MainActivity.kt
+++ app/src/main/java/com/rohitthebest/imagesliderusingviewpager2/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.rohitthebest.imagesliderusingviewpager2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.rohitthebest.imagesliderusingviewpager2.adapters.ImageViewPagerAdapter
+import com.rohitthebest.imagesliderusingviewpager2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var imageViewPagerAdapter: ImageViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //populating the list with some image urls
+        val imageUrlList = listOf(
+            "https://images.unsplash.com/photo-1621318164984-b06589834c91?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjM2OTk4MjI&ixlib=rb-1.2.1&q=80&w=1080",
+            "https://images.unsplash.com/photo-1621551122354-e96737d64b70?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjM2OTk4MjI&ixlib=rb-1.2.1&q=80&w=1080",
+            "https://images.unsplash.com/photo-1621616875450-79f024a8c42c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjM2OTk4MjI&ixlib=rb-1.2.1&q=80&w=1080",
+            "https://images.unsplash.com/photo-1621687947404-e41b3d139088?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjM2OTk4MjI&ixlib=rb-1.2.1&q=80&w=1080"
+        )
+
+        //initializing the adapter
+        imageViewPagerAdapter = ImageViewPagerAdapter(imageUrlList)
+
+        setUpViewPager()
+    }
+
+    private fun setUpViewPager() {
+
+        binding.viewPager.adapter = imageViewPagerAdapter
+
+        //set the orientation of the viewpager using ViewPager2.orientation
+        binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        //select any page you want as your starting page
+        val currentPageIndex = 1
+        binding.viewPager.currentItem = currentPageIndex
+
+        // registering for page change callback
+        binding.viewPager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+
+                    //update the image number textview
+                    binding.imageNumberTV.text = "${position + 1} / 4"
+                }
+            }
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // unregistering the onPageChangedCallback
+        binding.viewPager.unregisterOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {}
+        )
     }
 }
